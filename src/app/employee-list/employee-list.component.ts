f47855cfe5e62939613ca8bdf7e3f85b52e2ba65
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-21 23:48:49 +0100

    displaying data in employee-list.component file

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './employee-list.component.scss'
 })
 export class EmployeeListComponent {
+  employees!: Employee[];
 
+  constructor() {}
+   
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.employees = [
+      {
+        "id": 1,
+        "firstName": "Anouar",
+        "lastName": "LAACHIR",
+        "email": "anouar@gmail.com"
+      }, 
+      {
+        "id": 2,
+        "firstName": "Said",
+        "lastName": "BAALI",
+        "email": "said@gmail.com"
+      }
+    ]
+  }
 }
